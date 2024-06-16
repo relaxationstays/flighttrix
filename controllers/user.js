272cbc03a8841e11453ceb0f4ae9886ba2d014c2
relaxationstays <developer@relaxationstays.com>
2024-06-16 13:59:13 +0400

    first updadi

--- controllers/user.js
+++ controllers/user.js
@@ -121,8 +121,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
-        // secure: false,
-        // sameSite: "none",
+        secure: true,
+        sameSite: "none",
       })
       .status(200)
       .send({
